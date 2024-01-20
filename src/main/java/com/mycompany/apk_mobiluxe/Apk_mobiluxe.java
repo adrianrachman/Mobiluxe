3ad1a6376c50e5a7fdda683162a43f79127729af
adrianrachman <adrianrachman08@gmail.com>
2024-01-20 13:05:56 +0700

    Design GUI App

---
+++ src/main/java/com/mycompany/apk_mobiluxe/Apk_mobiluxe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apk_mobiluxe;
+
+/**
+ *
+ * @author adria
+ */
+public class Apk_mobiluxe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
